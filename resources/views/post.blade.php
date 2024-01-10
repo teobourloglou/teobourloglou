8f9a2655170c43495e12e19958f2f6ed7384e000
Theo2801 <theologos.bourloglou@gmail.com>
2024-01-10 14:28:26 +0200

    [feat] Make a Route and Link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias rerum optio iusto hic suscipit dolore, ut iste numquam, voluptatum laudantium asperiores nam sapiente dignissimos at facilis voluptatibus? Id, quas dolores.
+        </p>
+    </article>
+
+    <a href="/">Go back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias rerum optio iusto hic suscipit dolore, ut iste numquam, voluptatum laudantium asperiores nam sapiente dignissimos at facilis voluptatibus? Id, quas dolores.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias rerum optio iusto hic suscipit dolore, ut iste numquam, voluptatum laudantium asperiores nam sapiente dignissimos at facilis voluptatibus? Id, quas dolores.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias rerum optio iusto hic suscipit dolore, ut iste numquam, voluptatum laudantium asperiores nam sapiente dignissimos at facilis voluptatibus? Id, quas dolores.
+        </p>
+    </article>
+</body>
+</html>
