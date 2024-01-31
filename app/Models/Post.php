3be140562cb2e54d25f5838137dec3d05ecaff9e
Theo2801 <theologos.bourloglou@gmail.com>
2024-02-01 01:09:40 +0200

    [feat] A Few Tweaks and Consideration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -25,27 +25,37 @@ class Post {
 
     public static function find($slug) {
         // of all the blog posts, find the one with a slug that matches the one that was requested.
-        $posts = static::all();
 
-        return $posts->firstWhere('slug', $slug);
+        return static::all()->firstWhere('slug', $slug);
+    }
+
+    public static function findOrFail($slug) {
+        $post = static::find($slug);
+
+        if(! $post) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
     }
 
     public static function all() {
-        return cache()->Cache::rememberForever('posts.all', function () {
-            return collect(File::files(resource_path('posts/')))
-            ->map(function ($file) {
-                return YamlFrontMatter::parseFile($file);
-            })
-            ->map(function ($document) {
-                return new Post(
-                    $document->title,
-                    $document->slug,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                );
-            })
-            ->sortByDesc('date');
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path('posts')))
+                ->map(function ($file) {
+                    return YamlFrontMatter::parseFile($file);
+                })
+                ->map(function ($document) {
+                    return new Post(
+                        $document->title,
+                        $document->slug,
+                        $document->excerpt,
+                        $document->date,
+                        $document->body(),
+                    );
+                })
+                ->sortByDesc('date');
         });
     }
 }
+
