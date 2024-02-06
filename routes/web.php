277600e7d016fadea770f262426d49ea92c77260
Theo2801 <theologos.bourloglou@gmail.com>
2024-02-06 18:00:38 +0200

    [feat] Blade Components and CSS Grid

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Support\Facades\File;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +18,7 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->get()
     ]);
 });
 
@@ -34,5 +33,11 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 });
